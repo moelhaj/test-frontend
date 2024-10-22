0e002050f2d07a0d8592aca276c12f6d9cabb99b
Mohamed Elhaj <mo.elhaj@outlook.com>
2024-10-22 17:45:01 +0400

    hash router

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { store, persister } from "./store/store";
 import { PersistGate } from "redux-persist/integration/react";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import { Provider } from "react-redux";
 import { AppProvider } from "./hooks/useApp";
 import App from "./App.tsx";
@@ -12,9 +12,9 @@ createRoot(document.getElementById("root")!).render(
 	<Provider store={store}>
 		<PersistGate persistor={persister}>
 			<AppProvider>
-				<BrowserRouter>
+				<HashRouter>
 					<App />
-				</BrowserRouter>
+				</HashRouter>
 			</AppProvider>
 		</PersistGate>
 	</Provider>
